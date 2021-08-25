aa88ce1152814c624636e03df3b45ba9797196ab
Mathilda <63590524+mathildasmith0722@users.noreply.github.com>
2021-08-25 20:05:54 +0300

    Update project.c

--- project.c
+++ project.c
@@ -1,10 +1,10 @@
-#include "rally.h"
+#include "project.h"
 #include <stdlib.h>
 #include <string.h>
 
 
 struct Racer* add_driver(char *lastname, char *team, struct Racer *racerTimes, int numOfDrivers){
-    racerTimes = realloc(driverTimes,  numOfDrivers*sizeof(struct Racer));
+    racerTimes = realloc(racerTimes,  numOfDrivers*sizeof(struct Racer));
     racerTimes[numOfDrivers - 1].lastname = malloc((strlen(lastname)+1)*sizeof(char));
     racerTimes[numOfDrivers - 1].team = malloc((strlen(team)+1)*sizeof(char));
 
@@ -18,15 +18,15 @@ struct Racer* add_driver(char *lastname, char *team, struct Racer *racerTimes, i
     return racerTimes;
 }
 
-int update_time(char *lastname, int hours, int minutes, int seconds, struct Driver *racerTimes, int numOfDrivers){
+int update_time(char *lastname, int hours, int minutes, int seconds, struct Racer *racerTimes, int numOfDrivers){
     int num = 0;
     for(int i=0; i<numOfDrivers; i++){
         if(strcmp(racerTimes[i].lastname, lastname) == 0){
             racerTimes[i].racer_times.hours += hours;
             racerTimes[i].racer_times.minutes += minutes;
+            num=1;
             racerTimes[i].racer_times.seconds += seconds;
 
-            num = 1;
             if(racerTimes[i].racer_times.minutes>=60){
                 racerTimes[i].racer_times.hours += racerTimes[i].racer_times.minutes/60;
                 racerTimes[i].racer_times.minutes += racerTimes[i].racer_times.minutes%60;
@@ -45,54 +45,56 @@ int update_time(char *lastname, int hours, int minutes, int seconds, struct Driv
 }
 
 int comparison(const void *racer_time1, const void *racer_time2){
-    int time_1 = ((struct Racer *)racer_time1) -> racer_times.hours) + ((struct Racer *)racer_time1) -> racer_times.minutes) ((struct Racer *)racer_time1) -> racer_times.seconds)
-    int time_2 = ((struct Racer *)racer_time2) -> racer_times.hours) + ((struct Racer *)racer_time2) -> racer_times.minutes) ((struct Racer *)racer_time2) -> racer_times.seconds)
 
-    return (time_1 - time_2);
+    const struct Racer *time_11 = racer_time1;
+    const struct Racer *time_22 = racer_time2;
+    int time_1 = (time_11 -> racer_times.hours*60*60) + (time_11 -> racer_times.minutes*60) + (time_11-> racer_times.seconds);
+    int time_2 = (time_22 -> racer_times.hours*60*60) + (time_22 -> racer_times.minutes*60) + (time_22 -> racer_times.seconds);
+
+    return abs(time_1 - time_2);
 }
 
 int print_results(struct Racer *racerTimes, int numOfDrivers){
     qsort(racerTimes, numOfDrivers, sizeof(struct Racer), comparison);
     for(int i=0; i< numOfDrivers; i++){
-        printf("%s %s %d:%d:%d\n", racerTimes[i].last_name, racerTimes[i].team,
-        racerTimes[i].racer_time.hours, racerTimes[i].racer_time.minutes,
-        racerTimes[i].racer_time.seconds);
+        printf("%s %s %d:%d:%d\n", racerTimes[i].lastname, racerTimes[i].team, racerTimes[i].racer_times.hours, racerTimes[i].racer_times.minutes, racerTimes[i].racer_times.seconds);
     }
     return 0;
 }
 
 int save_results(char *filename, struct Racer *racerTimes, int numOfDrivers){
-    FILE *file_1 = fopen(filename, "a")
+    FILE *file_1 = fopen(filename, "a");
     if(!file_1){
         printf("File cant be opened");
         return -1;
     }
     for(int i=0; i<numOfDrivers; i++){
-        fprintf(file_1, "%s %s %s %d %d", racerTimes[i].lastname, racerTimes[i].team, &racerTimes[i].racer_times.hours, &racerTimes[i].racer_times.minutes, &racerTimes[i].racer_times.seconds);
+        fprintf(file_1, "%s %s %d %d %d", racerTimes[i].lastname, racerTimes[i].team, racerTimes[i].racer_times.hours, racerTimes[i].racer_times.minutes, racerTimes[i].racer_times.seconds);
     }
     fclose(file_1);
     return 0;   
 }
 
 int load_results(char *filename, struct Racer *racerTimes, int numOfDrivers){
-    FILE *file_1 = fopen(filename, "a")
+    FILE *file_1 = fopen(filename, "a");
     if(!file_1){
         printf("File cant be opened");
         return -1;
     }
     for(int i=0; i<numOfDrivers; i++){
-        fscan(file_1, "%s %s %s %d %d", racerTimes[i].lastname, racerTimes[i].team, &racerTimes[i].racer_times.hours, &racerTimes[i].racer_times.minutes, &racerTimes[i].racer_times.seconds);
+        fscanf(file_1, "%s %s %d %d %d", racerTimes[i].lastname, racerTimes[i].team, &racerTimes[i].racer_times.hours, &racerTimes[i].racer_times.minutes, &racerTimes[i].racer_times.seconds);
     }
     fclose(file_1);
     return 0;
 }
 
-int exit_program(char *lastname, char *team, int numOfDrivers){
+int exit_program(struct Racer *racerTimes, int numOfDrivers){
     for(int i=0; i<numOfDrivers; i++){
         free(racerTimes[i].lastname);
         free(racerTimes[i].team);
     }
     free(racerTimes);
+    return 0;
 }
 
 
@@ -111,13 +113,15 @@ int main(){
     printf("Enter a letter: \n");
 
     while(booll){
+
         scanf("%c", &letter);
+
         switch (letter) {
             case 'A':
-            scanf("%s %s", buffer1, buffer2);
-            numOfDrivers++;
-            racerTimes = add_driver(rep1, rep2, racerTimes, numOfDrivers);
-            break;
+                scanf("%s %s", rep1, rep2);
+                numOfDrivers++;
+                racerTimes = add_driver(rep1, rep2, racerTimes, numOfDrivers);
+                break;
                 
             case 'U':
                 scanf("%s %d %d %d", rep1, &hours, &minutes, &seconds);
@@ -140,11 +144,15 @@ int main(){
                 
             case 'Q':
                 booll = 0;
-                exit_program(rep1, rep2, numOfDrivers)
+                exit_program(racerTimes, numOfDrivers);
                 break;
                 
             default:
                 printf("You did not enter a correct input");
                 break;
-        }        
+
+        }
+    }   
 }
+
+
